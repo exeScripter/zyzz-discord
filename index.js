508952f8d083f975f1673d85b3d2db421b3ef732
exeScripter <oliwernoga@onet.pl>
2022-04-29 20:34:55 +0200

    Second Part

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const Discord = require("discord.js");
-require("dotenv").config()
+require("dotenv").config();
+
+const generateImage = require("./generateImage");
 
 const client = new Discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
 // When bot is ONLINE ==> then it types in console (Logged in as Zyzz | The Legend)
@@ -16,6 +19,18 @@ client.on("messageCreate", (message) => {
     if (message.content == "hi") {
         message.reply("Hello, brah")
     }
+})
+// makes a const with welcome channel id
+const welcomeChannelId = "969653068247465984"
+
+
+// welcome message
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content:`<@${member.id}> Welcome to the server brah!`,
+        files: [img]
+    })
 })
 
 client.login(process.env.TOKEN)
